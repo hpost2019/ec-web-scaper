18674f9c88ace36d14fcfb6b2c5240d5280ab4a9
Howard Post <hpost2012@gmail.com>
2020-05-24 16:10:17 -0700

    completed find email and phone

--- scraper.py
+++ scraper.py
@@ -5,15 +5,66 @@ import requests
 import re
 
 
+def print_lists(title, print_list):
+    """prints lists that are passed in with title"""
+    print(title, '\n')
+    for item in print_list:
+        print(item)
+    print('\n')
+    pass
+
+
+def remove_dups(my_list):
+    """removes duplicates from lists"""
+    results = []
+    for item in my_list:
+        if item not in results:
+            results.append(item)
+    return results
+
+
+def find_url(page_text):
+    """uses passed in text and finds all urls listed"""
+    found_urls = re.findall(
+        r"http[s]?://(?:[a-zA-Z]|[0-9]|"
+        r"[$-_@.&+]|[!*\(\),]|"
+        r"(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+        page_text)
+    if found_urls:
+        print_lists('URLs:', remove_dups(found_urls))
+    else:
+        print_lists('URLs:', ["None"])
+    pass
+
+
+def find_emails(page_text):
+    found_email = re.findall(
+        r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
+        page_text)
+    if found_email:
+        print_lists('EMAILS:', remove_dups(found_email))
+    else:
+        print_lists('EMAILS:', ["None"])
+    pass
+
+
+def find_phone_numbers(page_text):
+    found_phone_nums = re.findall(
+        r"1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})"
+        r"\W*([0-9]{4})(\se?x?t?(\d*))?",
+        page_text)
+    if found_phone_nums:
+        print_lists('PHONE NUMBERS:', remove_dups(found_phone_nums))
+    else:
+        print_lists('PHONE NUMBERS:', ["None"])
+
+
 def parse_url(response):
     """Accepts response from requests and parses response
     for information from website"""
-    found_url = re.findall(
-        r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-        , response.text)
-    print("URLs:\n")
-    for url in found_url:
-        print(url)
+    find_url(response.text)
+    find_emails(response.text)
+    find_phone_numbers(response.text)
 
 
 def main(args):
